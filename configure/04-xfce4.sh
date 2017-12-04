36ea7d61cb90e33486f55ff8a428f3ee59952d39
Thomas Perraut <mythom008@gmail.com>
2017-12-04 15:39:34 +0000

    Updated

--- configure/04-xfce4.sh
+++ configure/04-xfce4.sh
@@ -1,3 +1,3 @@
 yaourt -S xfce4 xfce4-goodies && \
-	echo "exec startxfce4" > ~/.xinitrc && \
-	echo -c "\e[1mxfce4 \e[32mDONE\e[39m"
+echo "exec startxfce4" > ~/.xinitrc && \
+echo -c "xfce4 mDONE"
