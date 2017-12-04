41a399e08f822e2c307094232f5336355132ccca
Thomas Perraut <mythom008@gmail.com>
2017-12-04 15:36:56 +0000

    Updated

--- configure/00-config.sh
+++ configure/00-config.sh
@@ -1,17 +1,12 @@
-
-# Emacs config
-cp ./configfiles/.emacs ~/ && \
-git clone https://github.com/ocaml/tuareg ~/.emacs.d/tuareg && \
-echo -c "\e[1mEmacs \e[32mDONE\e[39m"
 # Bashrc config
 cp ./configfiles/.bashrc ~/ && \
-echo -c "\e[1mBashrc \e[32mDONE\e[39m"
+echo "Bashrc DONE"
 # Vim config
 cp ./configfiles/.vimrc ~/ && \
 mkdir ~/.vim && \
 mkdir ~/.vim/plugin && \
 cp ./configfiles/42header.vim ~/.vim/plugin && \
-echo -c "\e[1mVimrc \e[32mDONE\e[39m" && \
+echo -c "Vimrc DONE" && \
 sleep 2
 # Disable BEEP
 sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf && \
