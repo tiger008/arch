8b6bab087b2bccc1b5794c91ece51b0ea2487d05
Thomas Perraut <mythom008@gmail.com>
2017-12-04 19:03:20 +0000

    adding copy of repo in new system

--- 00-Pre.sh
+++ 00-Pre.sh
@@ -2,4 +2,5 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup && \
 rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist && \
 pacstrap /mnt base base-devel && \
 genfstab -U /mnt >> /mnt/etc/fstab && \
+cp -r ../arch-master /mnt/home && \
 arch-chroot /mnt
