260cf88b2c8083cdf4cadd502ede2d5d82f2cadf
Thomas Perraut <mythom008@gmail.com>
2017-12-04 15:38:59 +0000

    Updated

--- configure/03-xorg.sh
+++ configure/03-xorg.sh
@@ -1,2 +1,2 @@
-yaourt -S xorg-server xorg-xinit xorg-server-utils xf86-input-synaptics && \
-	echo -c "\e[1mXorg \e[32mDONE\e[39m(installer pilote xf86-video-NAME)"
+yaourt -S xorg-server xorg-xinit && \
+echo "Xorg DONE (install your pilote xf86-video-NAME)"
