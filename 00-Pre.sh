709fe07ba058187eecc47d4e9370a7fac8c118a2
Thomas Perraut <mythom008@gmail.com>
2017-12-06 12:20:17 +0100

    Add -p for genfstab

--- 00-Pre.sh
+++ 00-Pre.sh
@@ -1,6 +1,6 @@
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup && \
 rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist && \
 pacstrap /mnt base base-devel && \
-genfstab -U /mnt >> /mnt/etc/fstab && \
+genfstab -U -p /mnt >> /mnt/etc/fstab && \
 cp -r ../arch-master /mnt/home && \
 arch-chroot /mnt
